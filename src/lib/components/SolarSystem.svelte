281e38e4d4c0595318948c1cbac6f7ea5e4ccfc2
Like4Schnitzel <rw21062006@gmail.com>
2023-11-24 21:36:57 +0100

    add twinkling stars

---
+++ src/lib/components/SolarSystem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TwinklingStars from "./TwinklingStars.svelte";
+</script>
+
+<body class="main">
+    <TwinklingStars />
+</body>
+
+<style>
+    .main {
+        height: 100%;
+        background-color: black;
+    }
+</style>
